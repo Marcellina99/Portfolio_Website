2d5961545a988e116d8f18138f1ba8d2d005ab02
Jindu Jindu <augusta.eshiowula@decagon.dev>
2022-08-31 16:07:16 +0100

    Added slash to the navbar links

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -11,11 +11,11 @@ const Nav = () => {
   const[activeNav, setActive] = useState('#')
   return (
     <nav>
-      <a href="#" onClick = {() => setActive('#')} className={activeNav === '#' ? 'active' : ''}><BiHomeHeart/></a>
-      <a href="#about" onClick = {() => setActive('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser/></a>
-      <a href="#experience" onClick = {() => setActive('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BiBook/></a>
-      <a href="#services" onClick = {() => setActive('#services')} className={activeNav === '#services' ? 'active' : ''}><RiServiceLine/></a>
-      <a href="#contact" onClick = {() => setActive('#contact')} className={activeNav === '#contact' ? 'active' : ''}><MdOutlineMessage/></a>
+      <a href="/#" onClick = {() => setActive('#')} className={activeNav === '#' ? 'active' : ''}><BiHomeHeart/></a>
+      <a href="/#about" onClick = {() => setActive('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser/></a>
+      <a href="/#experience" onClick = {() => setActive('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BiBook/></a>
+      <a href="/#services" onClick = {() => setActive('#services')} className={activeNav === '#services' ? 'active' : ''}><RiServiceLine/></a>
+      <a href="/#contact" onClick = {() => setActive('#contact')} className={activeNav === '#contact' ? 'active' : ''}><MdOutlineMessage/></a>
     </nav>
   )
 }
